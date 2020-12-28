de40f9e4e8507903dfa04497a34b4e18232ed52a
Mehdi Sebbah <sebbah.m.dev@gmail.com>
2020-12-28 13:50:44 +0100

    modification et suppression des fichiers inutiles

--- asset/footer.php
+++ asset/footer.php
@@ -6,11 +6,7 @@
 </main>
 </body>
 
-<script src="asset/js/scroll.js"></script>
-<script src="asset/js/fetch.js"></script>
-<script src="asset/js/click.js"></script>
-<script src="asset/js/load.js"></script>
-<script src="asset/js/menuBurger.js"></script>
+<script src="asset/js/app.js"></script>
 <script defer src="https://use.fontawesome.com/releases/v5.15.1/js/brands.js" integrity="sha384-GUtlu2Qit8cdodM5DbKnbDIWFJA8nWCVEwETZXY2xvKV1TFLtD/AL+bCOsPyh05M" crossorigin="anonymous"></script>
 <script defer src="https://use.fontawesome.com/releases/v5.15.1/js/fontawesome.js" integrity="sha384-v0OPwyxrMWxEgAVlmUqvjeEr48Eh/SOZ2DRtVYJCx1ZNDfWBfNMWUjwUwBCJgfO4" crossorigin="anonymous"></script>
 
--- asset/js/app.js
+++ asset/js/app.js
@@ -1,3 +1,52 @@
+const form = document.querySelector('.contact_form');
+//MODAL
+const modalContainer = document.querySelector('#modal_container');
+const modal = document.querySelector('.modal');
+const textModal = document.querySelector('#text_modal');
+const titleModal = document.querySelector('#title_modal');
+const closeModal = document.querySelector('#close_modal');
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    let formData = new FormData(form);
+    
+    fetch('contact.php', {
+        method: "POST",
+        body: formData,
+    })
+    .then(response => response.json())
+    .then(data => {
+        titleModal.innerHTML = data.title;
+        textModal.innerHTML = data.message;
+        modal.classList.add('open')
+        modalContainer.classList.add('open');
+    })
+})
+
+closeModal.addEventListener('click', () => {
+    modalContainer.classList.remove('open');
+})
+
+//EVENT ARROW UP FOR SCROLL AT THE HOME PAGE
+const arrow_up = document.querySelector('.arrow_up');
+
+arrow_up.addEventListener('click', () => {
+    window.scrollTo({
+        top: 0,
+        left:0,
+        behavior: "smooth",
+    })
+})
+
+//LOAD EVENT 
+const loader = document.querySelector('.loader');
+
+window.addEventListener('load', () => {
+    loader.classList.add('fondu-out');
+
+})
+
+//MENU BURGER AND BORDER WINDOW
 const header = document.querySelector('.header');
 const headerMobile = document.querySelector('.header_mobile');
 const headerNav = document.querySelector('.nav_header');
@@ -89,4 +138,41 @@ window.addEventListener('scroll', () => {
         textBorder.style.color = 'white';
 
     }
+})
+
+//EVENT SCROOL
+const titleAbout = document.querySelector(".about_me_title");
+const textAbout = document.querySelector(".about_me_text");
+const contentVoid = document.querySelector(".content_void");
+const bg = document.querySelector(".home");
+const skills = document.querySelectorAll(".skill");
+
+
+window.addEventListener('scroll', () => {
+
+    let scrollValue = (window.innerHeight + window.scrollY) / document.body.offsetHeight;
+    console.log(scrollValue)
+    bg.style.opacity = 1 - +window.pageYOffset/900;
+    
+    if(scrollValue > 0.28){
+        textAbout.style.opacity ='1';
+        textAbout.style.transform = 'none';
+    }
+
+    if(scrollValue > 0.50){
+        for(let skill of skills){
+            skill.style.opacity = '1';
+            skill.style.transform = 'none';
+        }
+    }
+
+    window.addEventListener('scroll',() => {
+        if(scrollValue < 0.36){
+            arrow_up.style.opacity ='0';
+            arrow_up.style.transition = '0.5s'
+        }else if(scrollValue > 0.36){
+            arrow_up.style.opacity = '1';
+            arrow_up.style.transition = '0.5s'
+        }
+    })
 })
